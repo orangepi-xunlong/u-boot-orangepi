a3300ab8257de3696ff8e5ee2def54ed4ce72a0c
leeboby <leeboby@aliyun.com>
2020-10-29 18:06:38 +0800

    Turn on the red light and turn off the green light at U-boot for H5

--- board/sunxi/board.c
+++ board/sunxi/board.c
@@ -610,12 +610,13 @@ void sunxi_board_init(void)
 {
 	int power_failed = 0;
 
-#ifdef CONFIG_MACH_SUN8I_H3
+#if defined(CONFIG_MACH_SUN8I_H3) || defined(CONFIG_MACH_SUN50I_H5)
 	/* turn off power LED (PL10) on H3 boards */
 	gpio_direction_output(SUNXI_GPL(10), 0);
 
 	gpio_direction_output(SUNXI_GPA(15), 1);
 	gpio_direction_output(SUNXI_GPA(17), 1);
+	gpio_direction_output(SUNXI_GPA(20), 1);
 #endif
 
 #ifdef CONFIG_SY8106A_POWER
