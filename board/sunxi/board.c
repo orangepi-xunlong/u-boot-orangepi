0c1f691ff2f40e77f1b5b05caa7ea6e4a09e0cd2
orangepi-xunlong <258384131@qq.com>
2022-04-12 14:01:02 +0800

    Turn on the red light and turn off the green light for H6

--- board/sunxi/board.c
+++ board/sunxi/board.c
@@ -681,6 +681,11 @@ void sunxi_board_init(void)
 {
 	int power_failed = 0;
 
+#if defined(CONFIG_MACH_SUN50I_H6)
+	gpio_direction_output(SUNXI_GPL(4), 1);
+	gpio_direction_output(SUNXI_GPL(7), 0);
+#endif
+
 #ifdef CONFIG_MACH_SUN8I_H3
 	/* turn on power LED (PL10) on H3 boards */
 	gpio_direction_output(SUNXI_GPL(10), 1);
