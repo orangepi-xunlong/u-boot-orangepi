0f7610abf7053e7b6cf9e7299aa1c0c1648efaa0
baiywt <baiywt_gj@163.com>
2024-04-24 14:03:22 +0800

    opi3b: Detect hw version via gpio4_c4 level to load matching dtb
    v1.1.1: rk3566-orangepi-3b.dtb
    v2.1:   rk3566-orangepi-3b-v2.dtb

--- board/rockchip/evb_rk3568/evb_rk3568.c
+++ board/rockchip/evb_rk3568/evb_rk3568.c
@@ -7,6 +7,8 @@
 #include <common.h>
 #include <dwc3-uboot.h>
 #include <usb.h>
+#include <dm.h>
+#include <asm/gpio.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
@@ -31,3 +33,34 @@ int board_usb_init(int index, enum usb_init_type init)
 	return dwc3_uboot_init(&dwc3_device_data);
 }
 #endif
+
+int rk_board_late_init(void)
+{
+	int ret, value;
+	unsigned int gpio = 148;
+	char dtb_name[32];
+	const char *model;
+
+	model = fdt_getprop(gd->fdt_blob, 0, "compatible", NULL);
+	if(strcmp(model, "rockchip,rk3566-orangepi-3b"))
+		return 0;
+
+	ret = gpio_request(gpio, "gpio4_c4");
+	if (ret && ret != -EBUSY) {
+		printf("gpio: requesting pin %u failed\n", gpio);
+		return -1;
+	}
+
+	gpio_direction_input(gpio);
+	value = gpio_get_value(gpio);
+	//printf("gpio value is %d\n", value);
+
+	if(value) {
+		char *fdtfile = env_get("fdtfile");
+		strcpy(dtb_name, fdtfile);
+		sprintf(dtb_name + strlen(dtb_name) - 4, "%s", "-v2.dtb");
+		env_set("fdtfile", dtb_name);
+	}
+
+	return 0;
+}
