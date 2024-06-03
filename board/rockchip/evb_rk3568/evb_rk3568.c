260ed1ba444bb0c1a96491e9c54219ce39fea01d
baiywt <baiywt_gj@163.com>
2024-06-03 14:49:02 +0800

    Update for opicm4 v1.4

--- board/rockchip/evb_rk3568/evb_rk3568.c
+++ board/rockchip/evb_rk3568/evb_rk3568.c
@@ -9,6 +9,10 @@
 #include <usb.h>
 #include <dm.h>
 #include <asm/gpio.h>
+#include <asm/arch/resource_img.h>
+#include <sysmem.h>
+#include <fdt_support.h>
+#include <asm/io.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
@@ -34,31 +38,45 @@ int board_usb_init(int index, enum usb_init_type init)
 }
 #endif
 
+#define OFFSET_TO_BIT(bit)     (1UL << (bit))
+
+#define REG_L(R)       (R##_l)
+#define REG_H(R)       (R##_h)
+#define READ_REG(REG)  ((readl(REG_L(REG)) & 0xFFFF) | \
+                       ((readl(REG_H(REG)) & 0xFFFF) << 16))
+#define WRITE_REG(REG, VAL)    \
+{\
+       writel(((VAL) & 0xFFFF) | 0xFFFF0000, REG_L(REG)); \
+       writel((((VAL) & 0xFFFF0000) >> 16) | 0xFFFF0000, REG_H(REG));\
+}
+#define CLRBITS_LE32(REG, MASK)        WRITE_REG(REG, READ_REG(REG) & ~(MASK))
+#define SETBITS_LE32(REG, MASK)        WRITE_REG(REG, READ_REG(REG) | (MASK))
+
 int rk_board_late_init(void)
 {
-	int ret, value;
-	unsigned int gpio = 148;
+	int value;
 	char dtb_name[32];
+	char dtb_ver[10];
 	const char *model;
 
 	model = fdt_getprop(gd->fdt_blob, 0, "compatible", NULL);
-	if(strcmp(model, "rockchip,rk3566-orangepi-3b"))
+	if(!strcmp(model, "rockchip,rk3566-orangepi-3b")) {
+		strcpy(dtb_ver, "-v2.dtb");
+	} else if(!strcmp(model, "rockchip,rk3566-orangepi-cm4")) {
+		strcpy(dtb_ver, "-v1.4.dtb");
+	} else
 		return 0;
 
-	ret = gpio_request(gpio, "gpio4_c4");
-	if (ret && ret != -EBUSY) {
-		printf("gpio: requesting pin %u failed\n", gpio);
-		return -1;
-	}
+#define GPIO4_BASE      0xfe770000
+	struct rockchip_gpio_regs *regs = (void *)GPIO4_BASE;
 
-	gpio_direction_input(gpio);
-	value = gpio_get_value(gpio);
-	//printf("gpio value is %d\n", value);
+	CLRBITS_LE32(&regs->swport_ddr, OFFSET_TO_BIT(20));
+	value = readl(&regs->ext_port) & OFFSET_TO_BIT(20) ? 1 : 0;
 
 	if(value) {
 		char *fdtfile = env_get("fdtfile");
 		strcpy(dtb_name, fdtfile);
-		sprintf(dtb_name + strlen(dtb_name) - 4, "%s", "-v2.dtb");
+		sprintf(dtb_name + strlen(dtb_name) - 4, "%s", dtb_ver);
 		env_set("fdtfile", dtb_name);
 	}
 
